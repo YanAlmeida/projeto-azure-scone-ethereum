cad012ee9955eee5625fd36a21b323a4a1dbf1ba
Yan Almeida <yan.almeida91@hotmail.com>
2023-08-02 20:41:11 -0300

    feat: criacao de classe para representacao do contrato inteligente

--- code/tee_code/app/smart_contract_access.py
+++ code/tee_code/app/smart_contract_access.py
@@ -1,20 +1,38 @@
+import os
 from web3 import Web3, Account
+from web3.contract import Contract
+from mnemonic import Mnemonic
+from hdwallet import HDWallet
 
-# Connect to Ethereum
-w3 = Web3(Web3.HTTPProvider('http://ip_of_blockchain_instance:8545'))
+class SmartContract:
+    
+    _account: Account
+    _contract: Contract
+    
+    def __init__(self, account: Account, contract: Contract):
+        self._account = account
+        self._contract = contract
 
-# Get the account information
-private_key = 'YOUR_PRIVATE_KEY'
-account = Account.privateKeyToAccount(private_key)
 
-# ABI and address of the smart contract
-contract_abi = '[ABI_HERE]'
-contract_address = '0xAddressOfYourSmartContract'
+MNEMONIC_WORDS = os.environ.get("MNEMONIC")
+BLOCKCHAIN_ADDRESS = os.environ.get("BLOCKCHAIN_ADDRESS")
+CONTRACT_ABI = os.environ.get("CONTRACT_ABI")
+CONTRACT_ADDRESS = os.environ.get("CONTRACT_ADDRESS")
 
-# Create contract object
-contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+MNEMO = Mnemonic.to_seed(MNEMONIC_WORDS, passphrase="")
+WALLET = HDWallet()
+WALLET.from_seed(MNEMO.hex())
 
-# Read data from the contract (replace 'data' with the actual function you want to call)
-data = contract.functions.data().call()
+W3 = Web3(Web3.HTTPProvider(BLOCKCHAIN_ADDRESS))
 
-print('Data from smart contract:', data)
+
+def main():
+    account = Account.from_key(WALLET.private_key())
+    contract = W3.eth.contract(address=CONTRACT_ADDRESS, abi=CONTRACT_ABI)
+    
+    smartContract = SmartContract(account, contract)
+    print(smartContract.__dict__)
+
+if __name__ == "__main__":
+    main()
+        
