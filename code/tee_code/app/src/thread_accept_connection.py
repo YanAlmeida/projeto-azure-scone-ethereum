66977d94894edbd48ecc97f4590cdf33637bc902
Yan Almeida <yan.almeida91@hotmail.com>
2023-12-11 23:23:35 -0300

    refactor: refatora comunicacao entre tee e untrusted code para transmissao de dados maiores

--- code/tee_code/app/src/thread_accept_connection.py
+++ code/tee_code/app/src/thread_accept_connection.py
@@ -3,6 +3,27 @@ from typing import List, Dict, Any
 from src.user_types import Job
 
 
+def receive_data(conn: 'Connection') -> str:
+    """
+    Função que recebe os dados a partir de uma conexão TCP
+    :param conn: Objeto representando a conexão
+    :return: Dados recebidos na forma de string
+    """
+    buffer_size = 64*1024  # 64 KB
+    delimiter = '#END_OF_TRANSMISSION#'
+    data = ''
+    while True:
+        chunk = conn.recv(buffer_size).decode('utf-8')
+        if not chunk:
+            # Connection closed
+            break
+        data += chunk
+        if data.endswith(delimiter):
+            # End of transmission
+            break
+    return data.rstrip(delimiter)
+
+
 def generate_count_chars(messages: List[Job]) -> List[Dict[str, Any]]:
     """
     Função que processa os dados recebidos
@@ -24,7 +45,7 @@ def generate_count_chars(messages: List[Job]) -> List[Dict[str, Any]]:
     return [count_chars(message) for message in messages]
 
 
-def accept_connection(conn):
+def accept_connection(conn: 'Connection'):
     """
     Função para recebimento de dados e retorno de função para thread de processamento/resposta
     :return:
@@ -32,8 +53,8 @@ def accept_connection(conn):
     
     def process_data():
         with conn:
-            data = conn.recv(1024)
-            json_data = json.loads(data.decode('utf-8'))
+            data = receive_data(conn)
+            json_data = json.loads(data)
             result = json.dumps(generate_count_chars(json_data)).encode("utf-8")
             conn.sendall(result)
 
