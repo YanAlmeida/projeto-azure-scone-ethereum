266833f3154cc062e78ba74318cf00f9ecf1aaf4
Yan Almeida <yan.almeida91@hotmail.com>
2024-01-29 01:03:28 -0300

    fix: corrige nome de funcao alterada

--- code/untrusted_code/app/main.py
+++ code/untrusted_code/app/main.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 
     # Monitora constantemente os eventos da blockchain , aguardando notificacoes de jobs alocados para ela
     while True:
-        jobs = get_contract().getJobsMachine()
+        jobs = get_contract().pollJobs()
         if len(jobs) > 0:
             thread_get_jobs = threading.Thread(target=get_and_process_jobs(jobs))
             thread_get_jobs.start()
