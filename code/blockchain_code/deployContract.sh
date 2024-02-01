b1bb5a5364566c294ee37981e3200cef59f2616c
Yan Almeida <yan.almeida91@hotmail.com>
2024-02-01 01:57:22 -0300

    chore: define gas price do ganache

--- code/blockchain_code/deployContract.sh
+++ code/blockchain_code/deployContract.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start Ganache in the background
-ganache --server.host 0.0.0.0 --wallet.mnemonic "${MNEMONIC}" --wallet.hdPath "${DERIVATION_PATH}" --wallet.defaultBalance "${INITIAL_BALANCE}" -a 1000 --miner.blockTime 15 --miner.blockGasLimit 0x1C9C380 &
+ganache --server.host 0.0.0.0 --wallet.mnemonic "${MNEMONIC}" --wallet.hdPath "${DERIVATION_PATH}" --wallet.defaultBalance "${INITIAL_BALANCE}" -a 1000 --miner.blockTime 15 --miner.blockGasLimit 0x1C9C380 --miner.defaultGasPrice 0x174876e800 &
 
 # Wait for Ganache to initialize
 echo "Waiting for Ganache to initialize..."
